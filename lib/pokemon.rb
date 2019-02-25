cffdeb17b618a2b1cbf371da84130e2cd615f983
moranja <jeffadammoran@gmail.com>
2019-02-25 17:02:28 -0600

    refactored

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,41 +1,30 @@
 require 'pry'
 require 'sqlite3'
+
 class Pokemon
-  attr_accessor :id, :hp,  :name, :type, :db
-  def initialize(db, hp =60)
-    @db = db
-    self.name = name
-    self.hp = hp
+  attr_accessor :id, :name, :type, :db, :hp
+  def initialize(db)
+    self.db = db
   end
 
-  def self.save(name, type, db) 
-    #new_pokemon = Pokemon.new(db)
-    #binding.pry
-      
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",[name, type]) 
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",[name, type])
   end
 
- 
-    
   def self.find(id, db)
     pokemon_from_db = Pokemon.new(db)
-    x = db.execute("SELECT * FROM pokemon WHERE id = #{id} ")
-    new_pokemon = x[0]
-    pokemon_from_db.id = new_pokemon[0]
-    #binding.pry
-    pokemon_from_db.name = new_pokemon[1]
-    pokemon_from_db.type = new_pokemon[2]
-    pokemon_from_db.hp = new_pokemon[3]
-    return pokemon_from_db
+    db.results_as_hash = TRUE
+    found_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ? ", [id])[0]
+    pokemon_from_db.id = found_pokemon["id"]
+    pokemon_from_db.name = found_pokemon["name"]
+    pokemon_from_db.type = found_pokemon["type"]
+    pokemon_from_db.hp = found_pokemon["hp"]
+    return pokemon_from_db #this feels so clunky...
   end
 
-  def alter_hp(hp=60, db) 
-    if self.name == 'Pikachu'
-      @db.execute("UPDATE pokemon SET hp = 59 WHERE name = (?)", [self.name])
-    elsif
-      @db.execute("UPDATE pokemon SET hp = 0 WHERE name = (?)", [self.name])
-    end
+  def alter_hp(hp, db)
+    @db.execute("UPDATE pokemon SET hp = (?) WHERE name = (?)", [hp, self.name])
   end
 
 
-end 
+end
